19940327502806a9e197e4dedac826b991675918
Matheusdamaceno <damaceno.martins@hotmail.com>
2021-04-12 17:49:30 +0000

    edição

---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+
+export function TodoItem({todo, index, removeTodo, completeTodo}) {
+    return (
+        <div style={{textDecoration: todo.isCompleted ? "line-through" : ""}}>
+            {todo.text}
+            <button onClick={() => removeTodo(index)}>X</button>
+            <button onClick={() => completeTodo(index)}>Terminada</button>
+        </div>
+
+    );
+}
